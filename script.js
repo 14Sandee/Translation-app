51e73bab6cfb8d76aacf38993fd63b50cf9d5acd
14Sandee <55476652+14Sandee@users.noreply.github.com>
2021-05-24 13:13:48 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 var btn = document.querySelector("#btn-element");
 var txtInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
+function getTranslatedURL(text){
+    return serverURL + "?text=" + text;
+}
 
-btn.addEventListener("click", function clickedHandler() {
-    console.log("clicked");
-    console.log("input:", txtInput.value);
-})
+function errorHandling(error){
+    alert("Something went wrong, Please try again later");
+}
+function clickedHandler(){
+    var inputText = txtInput.value;
+
+    fetch(getTranslatedURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandling)
+
+};
+
+btn.addEventListener("click", clickedHandler)
